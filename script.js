b1c5dd66e62be18f4c37aca08060ab8c6787f66b
ТвойНик <@.ru>
2026-03-29 13:38:42 +0300

    .

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function greet() {
     alert("Привет! Это сайт про винил новой школы");
 }
+
+function askFavorite() {
+    let track = prompt("Какой твой любимый трек этого исполнителя?");
+    if (track) {
+        alert("Отличный выбор! «" + track + "» — это круто.");
+    } else {
+        alert("Ты не ввел трек :(");
+    }
+}
